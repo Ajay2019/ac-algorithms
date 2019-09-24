e36dfa6450ce06404821431074041b1df261841e
Ajay <ajay@anniecannons.com>
2019-09-24 10:13:43 -0700

    DNA code done- 9/24/2019

--- dnaPairing.js
+++ dnaPairing.js
@@ -9,12 +9,38 @@
 //For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
 
 //The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+//Input is a string and output is a nested array 
+// We need the split and push to seperate and push the new array 
+//Everytiime the loop runs, it needs to push the new paired element into the new array
+
+
 
 function pairElement(str) {
-  //your code here
-}
-
-pairElement("GCG");
+  var array=str.split('');// Splitting the input 
+   console.log(array);// to check if the split is correct
+  var newArray=[];//For pushing the new array and return
+ 
+  for(i=0;i<array.length;i++){
+    if(array[i]==="G"){
+      newArray.push(["G","C"]);
+    }
+    else if(array[i]==="A"){
+      newArray.push(["A","T"]);
+    }
+    else if(array[i]==="T"){
+      newArray.push(["T","A"]);
+    }
+    else{
+      newArray.push(["C","G"])
+    }
+  }
+   
+ return newArray;
+   
+ }
+ 
+ var final = pairElement("GCG");
+ console.log(final);
 
 //TEST CASES
 //pairElement("ATCGA") should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]].
